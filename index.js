1d7c282e44ec09d97298703f7aa7d8435abaf3f6
Edward Chisaka <chisaka@Edwards-MacBook-Air.local>
2022-08-29 11:09:52 +0300

    All passing

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
-
+let log = console.log
 // Code your solution here
+
+const totalBatteries = batteryBatches.reduce((previousValue, currentValue) => previousValue + currentValue);
+
+log(totalBatteries);
